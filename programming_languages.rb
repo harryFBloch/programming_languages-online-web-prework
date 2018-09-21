b1eae42c4d0ec889892f41bd2b9a361bd8c55f0e
Harry Bloch <harryfbloch@gmail.com>
2018-09-21 19:58:22 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,7 +1,9 @@
 def reformat_languages(languages)
   new_hash = {}
   languages.each { |style, languageInfo| 
-    languageInfo.each
+    languageInfo.each { |key, value|
+      new_hash[key] = 
+    }
   }
 end
 
